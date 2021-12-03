5fed41201e32fde0f77dce57516cf58ae2d146ef
LukeMcCann <developer-lukemccann@outlook.com>
2021-12-03 20:13:48 +0000

    Improve with less iterations

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -13,9 +13,9 @@
 function bubbleSort(nums) {
   do {
       swapped = false;
-      for (let i = 0; i < nums.length; i++) {
+      for (let i = 0; i < nums.length-1; i++) {
           if (nums[i] > nums[i+1]) {
-              let temp = nums[i];
+              const temp = nums[i];
               nums[i] = nums[i+1];
               nums[i+1] = temp;
 

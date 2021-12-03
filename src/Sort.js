37d064ff8ffa38e59e7a0d2ab81a85b4fd25a672
LukeMcCann <developer-lukemccann@outlook.com>
2021-12-03 20:32:11 +0000

    Add snapshot code

--- src/Sort.js
+++ src/Sort.js
@@ -4,14 +4,37 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
+function sort(array, algorithm = 'bubbleSort') {
   // do cool stuff here
 
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
+  if (algorithm === 'bubbleSort') {
+    array = bubbleSort(array);
+  }
+
   snapshot(array);
 }
+
+function bubbleSort(nums) {
+  let swapped = false;
+  do {
+      swapped = false;
+      for (let i = 0; i < nums.length-1; i++) {
+          snapshot(nums);
+          if (nums[i] > nums[i+1]) {
+              const temp = nums[i];
+              nums[i] = nums[i+1];
+              nums[i+1] = temp;
+              swapped = true;
+          }
+      }
+  } while(swapped);
+
+  return nums;
+}
+
 
 export default function SortComponent() {
   clear();
